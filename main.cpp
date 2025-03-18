40e63be9ab0c7ad5605e75c3d4aadb440cc04f64
Deniz Gunes <denizlg24@gmail.com>
2025-03-18 13:30:15 +0000

    Fixed commandregistry not reseting

--- main.cpp
+++ main.cpp
@@ -12,18 +12,18 @@
 
 
 int main(int argc, char* argv[]) {
-    MapFactory factory;
-    CommandRegistry registry;
-    registry.getContext()->set("map_factory",factory);
-    registry.registerCommand("read_cities",new ReadCities());
-    registry.registerCommand("read_roads",new ReadRoads());
-    registry.registerCommand("help",new HelpCommand());
-    registry.registerCommand("commands",new HelpCommand());
-    registry.registerDependency("read_roads", "read_cities");
+    auto *factory = new MapFactory();
+    auto *registry = new CommandRegistry();
+    registry->getContext()->set("map_factory",*factory);
+    registry->registerCommand("read_cities",new ReadCities());
+    registry->registerCommand("read_roads",new ReadRoads());
+    registry->registerCommand("help",new HelpCommand());
+    registry->registerCommand("commands",new HelpCommand());
+    registry->registerDependency("read_roads", "read_cities");
     if (argc == 3) {
-        CLI::runFromFile(registry, argv[1], argv[2]);
+        CLI::runFromFile(*registry, argv[1], argv[2]);
     } else {
-        CLI::runInteractive(registry);
+        CLI::runInteractive(*registry);
     }
     return 0;
 }
